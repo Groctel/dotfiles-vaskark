b776da801b4a6a33f3b5e6d054209526e258dc70
vaskark <vaskark@gmail.com>
2019-08-30 16:28:20 -0400

    update

---
+++ .config/glava/wave.glsl
@@ -0,0 +1,10 @@
+/* Min (vertical) line thickness */
+#define MIN_THICKNESS 1
+/* Max (vertical) line thickness */
+#define MAX_THICKNESS 6
+/* Base color to use, distance from center will multiply the RGB components */
+#define BASE_COLOR @fg:vec4(0.239, 0.553, 0.569, 1)
+/* Amplitude */
+#define AMPLIFY 500
+/* Outline color */
+#define OUTLINE @bg:vec4(0.15, 0.15, 0.15, 1)
