7d41a20c81f797d1e753160d4917ca2a17d3cad4
vaskark <vaskark@gmail.com>
2019-09-02 01:37:51 -0400

    update

--- .local/bin/initscreen-left.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# 2nd monitor left of screen
-
-xrandr --output HDMI-2 --mode 1920x1080 --left-of eDP-1
--- .local/bin/initscreen-right.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# 2nd monitor right of screen
-
-xrandr --output HDMI-2 --mode 1920x1080 --right-of eDP-1
