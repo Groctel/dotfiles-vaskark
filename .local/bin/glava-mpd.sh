ab2f6a6f4827a2ccd07a14c7ad94aeeccf758912
vaskark <vaskark@gmail.com>
2019-09-02 01:26:27 -0400

    update

---
+++ .local/bin/glava-mpd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Terminate
+killall -q glava
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x glava >/dev/null; do sleep 1; done
+
+# Launch
+glava --desktop --audio=fifo &
