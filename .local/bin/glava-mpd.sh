9771391525b3767a5a128b5b9e37272ecb7664db
vaskark <vaskark@gmail.com>
2019-11-30 01:47:11 -0500

    updates

--- .local/bin/glava-mpd.sh
+++ .local/bin/glava-mpd.sh
@@ -4,7 +4,7 @@
 killall -q glava
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x glava >/dev/null; do sleep 1; done
+#while pgrep -u $UID -x glava >/dev/null; do sleep 1; done
 
 # Launch
 glava --desktop --audio=fifo &
--- .local/bin/redshift.sh
+++ .local/bin/redshift.sh
@@ -4,7 +4,7 @@
 killall -q redshift
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
+#while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
 
 # Launch
 redshift &
