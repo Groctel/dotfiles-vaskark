5fe9855da6fdb672408d2921b38933e9eea0706e
vaskark <vaskark@gmail.com>
2019-12-01 00:41:40 -0500

    updates

--- work/sowm/config.h
+++ work/sowm/config.h
@@ -6,7 +6,7 @@
 const char* menu[]    = {"dmenu_run",      0};
 const char* rofi[]    = {"rofi", "-show", "drun", "-theme", "themes/drun.rasi",      0};
 const char* term[]    = {"kitty",             0};
-const char* sshot[]   = {"screenshot.sh", "full",            0};
+const char* shot[]   = {"screenshot.sh", "full",            0};
 const char* briup[]   = {"light", "-A", "5", 0};
 const char* bridown[] = {"light", "-U", "5", 0};
 const char* voldown[] = {"pactl", "set-sink-volume", "0", "-5%",         0};
@@ -29,7 +29,7 @@ static struct key keys[] = {
 
     {MOD|ShiftMask,	XK_d,	   	run, {.com = menu}},
     {MOD, 	    	XK_d,      	run, {.com = rofi}},
-    {MOD,           	XK_p,      	run, {.com = sshot}},
+    {MOD,           	XK_Print,      	run, {.com = shot}},
     {MOD,           	XK_Return, 	run, {.com = term}},
 
     {MOD, 		XK_Escape, 	run, {.com = lock}},
