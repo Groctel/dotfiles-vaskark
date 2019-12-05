c8500f054005258556b2e55fee2140f2bee08178
vaskark <vaskark@gmail.com>
2019-12-05 14:41:00 -0500

    updates

--- .local/bin/lemonbar.sh
+++ .local/bin/lemonbar.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-( sleep 1s && bash lemonbar-stuff.sh | lemonbar -p -B "$color0" -F "$color8" -f "Monospace"-8 -f "Font Awesome 5 Free Regular"-8 -g 1910x28+5+5) &
+( sleep 1s && bash lemonbar-script1.sh | lemonbar -p -B "$color0" -F "$color8" -f Hermit-8 -f "Font Awesome 5 Free Regular"-8 -g 1910x26+5+5) &
