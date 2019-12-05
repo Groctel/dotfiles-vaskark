8a193f88a8826bee94458acdcb65f2c24d62b827
vaskark <vaskark@gmail.com>
2019-12-05 14:45:35 -0500

    updates

--- .local/bin/lemonbar.sh
+++ .local/bin/lemonbar.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-( sleep 1s && bash lemonbar-script1.sh | lemonbar -p -B "$color0" -F "$color8" -f Hermit-8 -f "Font Awesome 5 Free Regular"-8 -g 1910x26+5+5) &
+( sleep 1s && bash lemonbar-script1.sh | lemonbar -p -B "$color0" -F "$color8" -f "Iosevka Custom"-8 -f "Font Awesome 5 Free Regular"-8 -g 1910x26+5+5) &
