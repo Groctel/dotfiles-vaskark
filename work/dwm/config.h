7f9d9da64953064d61afb3cebb0f3456cad8d770
vaskark <vaskark@gmail.com>
2019-12-09 20:20:47 -0500

    updates

--- work/dwm/config.h
+++ work/dwm/config.h
@@ -10,10 +10,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int gappx    = 10;        /* gaps between windows */
 static const char *fonts[]          = { "Iosevka Custom:size=7.5", "Symbols Nerd Font:size=8" };
-static const char dmenufont[]       = "Iosevka Custom:size=8";
-static const char *upvol[]          = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
-static const char *downvol[]        = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
-static const char *mutevol[]        = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+/*static const char dmenufont[]       = "Iosevka Custom:size=8";*/
+static const char *upvol[]          = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
+static const char *downvol[]        = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
+static const char *mutevol[]        = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK", "toggle",  NULL };
 static const char col_gray1[]       = "#282F37"; /* bar bg */
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb"; /* bar fg */
@@ -65,15 +65,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[] = { "rofi", "-no-lazy-grab", "-show", "drun", NULL };
+static const char *dmenucmd[] = { "dmenu.sh", NULL };
+static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *lockcmd[] = { "betterlockscreen", "-l", "pixel", NULL};
+static const char *logoutcmd[] = { "killall", "-9", "dwm", NULL};
 static const char *rebootcmd[] = {"systemctl", "reboot", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -107,7 +108,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY,                       XK_Delete, quit,           {0} },
-        { MODKEY|ShiftMask,             XK_Delete, spawn,          {.v = rebootcmd } },
+/*	{ MODKEY|ShiftMask,		XK_Delete, spawn,	   {.v = rebootcmd } },*/
         { MODKEY,			XK_Escape, spawn,	   {.v = lockcmd } },
 	{ MODKEY,                       XK_F11,    spawn,          {.v = downvol } },
 	{ MODKEY,                       XK_F10,    spawn,          {.v = mutevol } },
