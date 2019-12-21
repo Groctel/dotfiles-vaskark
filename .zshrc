cbe666ee3e9043e638e8392ec1f7b9ef3f12027a
vaskark <vaskark@gmail.com>
2019-12-20 23:07:03 -0500

    updates

--- .zshrc
+++ .zshrc
@@ -194,4 +194,4 @@ PF_ALIGN=""
 PF_ASCII="ubuntu"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+#[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- work/st/config.h
+++ work/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=10:antialias=true:autohint=true";
+static char *font = "Monospace:size=7.5:antialias=true:autohint=true";
 static char *font2[] = { "Typicons:size=10:antialias=true:autohint=true" };
 static int borderpx = 20;
 
