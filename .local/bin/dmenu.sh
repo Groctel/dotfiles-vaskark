146b5615949faa8ed82e1fdee106e3f802f4fd80
vaskark <vaskark@gmail.com>
2020-01-14 20:18:12 -0500

    updates

--- .local/bin/dmenu.sh
+++ .local/bin/dmenu.sh
@@ -8,9 +8,9 @@
 # -sb "$color1"
 # dmenu_run -fn 'Iosevka Custom-8' -h 26 -p ' dmenu: ' -i -nb "$color0" -nf "$color15" -sb "$color4" -sf "#000000" -dim 0.5 -x 10 -y 10 -w 1900
 
-border=5
+b=5
 width=$(xdotool getdisplaygeometry | awk '{print $1}')
-w=$(( width-(2*border) ))
+w=$(( width-(2*b) ))
 
 # dmenu2
 dmenu_run \
@@ -23,6 +23,6 @@ dmenu_run \
 -sb "$color3" \
 -sf "#000000" \
 -dim 0.2 \
--x $border \
--y $border \
+-x $b \
+-y $b \
 -w $w \
