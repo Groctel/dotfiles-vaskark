07c4c781cd399537175fe30f76b2ad0bdda0dbdc
vaskark <vaskark@gmail.com>
2020-02-06 10:12:16 -0500

    updates

--- .local/bin/exit.sh
+++ .local/bin/exit.sh
@@ -5,7 +5,7 @@
 MENU="$(rofi -sep "|" -dmenu -i -p 'System' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 -font "Iosevka Custom 8" <<< " Lock| Logout| Reboot| Shutdown")"
             case "$MENU" in
                 *Lock) betterlockscreen -l pixel ;;
-                *Logout) killall sowm.sh ;;
+                *Logout) pkill -15 -t tty"$XDG_VTNR" Xorg ;;
                 *Reboot) systemctl reboot ;;
                 *Shutdown) systemctl -i poweroff
             esac
