0407ab96c66fb4dad04d24105198dfedbd25c6d1
vaskark <vaskark@gmail.com>
2020-02-15 21:43:03 -0500

    updates

---
+++ .local/bin/dmenu-fb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# vars
+b=5
+width=$(xdotool getdisplaygeometry | awk '{print $1}')
+w=$(( width-(2*b) ))
+
+# dmenu2
+dmenu_run \
+-fn 'Iosevka Custom-8' \
+-h 28 \
+-p ' dmenu: ' \
+-i \
+-nb "#ffffff" \
+-nf "#000000" \
+-sb "#E6804E" \
+-sf "#000000" \
+-dim 0.2 \
+-x $b \
+-y $b \
+-w $w \
