97a879c9f33aa66b44d072037282f1329dd8cff4
vaskark <vaskark@gmail.com>
2020-02-18 11:56:27 -0500

    updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
 
-# Launch top and bottom
-echo "---" | tee -a /tmp/polybar-top.log /tmp/polybar-bottom.log
-polybar -r top >>/tmp/polybar-top.log 2>&1 &
-polybar -r bottom >>/tmp/polybar-bottom.log 2>&1 &
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch bar1 and bar2
+polybar main -c ~/.config/polybar/config.ini &
