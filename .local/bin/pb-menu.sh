1b3873354c1c9539ec3df3ca2dd61e85218fa0ed
vaskark <vaskark@gmail.com>
2020-03-03 00:00:18 -0500

    updates

--- .local/bin/pb-menu.sh
+++ .local/bin/pb-menu.sh
@@ -2,11 +2,11 @@
 
 ## Polybar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 5 -font "scientifica bold 8" <<< "Default|EWMH|No-EWMH|Colors|OFF")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 5 -font "scientifica bold 8" <<< "Default|2|2b|Colors|OFF")"
             case "$MENU" in
                 *Default) polybar.sh ;;
-                *EWMH) polybar-ewmh.sh ;;
-                *No-EWMH) polybar-no-ewmh.sh ;;
+                *2) polybar-2.sh ;;
+                *2b) polybar-2b.sh ;;
                 *Colors) polybar-p11.sh ;;
 		*OFF) pkill polybar
             esac
--- .local/bin/pb-toggle.sh
+++ .local/bin/pb-toggle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Toggle polybar:
 # If on, turn off. If off, start.
-xdotool search polybar &>/dev/null && (ps x | grep "polybar" | grep -v grep | awk '{print $1}' | xargs kill) || polybar-no-ewmh.sh &
+xdotool search polybar &>/dev/null && (ps x | grep "polybar" | grep -v grep | awk '{print $1}' | xargs kill) || polybar-2b.sh &
