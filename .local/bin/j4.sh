18e0ee2ff86987161f69904d9aea12ebf61b12f3
vaskark <vaskark@gmail.com>
2020-03-03 00:06:00 -0500

    updates

--- .local/bin/j4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Import wal colors 
-. "${HOME}/.cache/wal/colors.sh"
-
-j4-dmenu-desktop --dmenu="dmenu -i -h 30 -q -r -p ' find: ' -fn 'scientifica-11:bold' -x 5 -y 5 -w 1910 -dim 0.25 -o 0.8"
