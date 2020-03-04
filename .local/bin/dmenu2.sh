b53028964d304c3397eaaf440d7be6f3f9101a89
vaskark <vaskark@gmail.com>
2020-03-04 13:46:39 -0500

    updates

---
+++ .local/bin/dmenu2.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Import wal colors 
+. "${HOME}/.cache/wal/colors.sh"
+
+# vars for xyw
+b=5
+width=$(xdotool getdisplaygeometry | awk '{print $1}')
+w=$(( width-(2*b) ))
+
+# dmenu
+dmenu_run \
+-p ' dmenu ' \
+-fn 'scientifica-11:bold' \
+-h 30 \
+-nb "$color3" \
+-nf "$color0" \
+-sb "$color0" \
+-sf "$color3" \
+-x $b \
+-y $b \
+-w $w \
+-dim 0.25 \
+-o 0.90 \
+#-z \
+
+# border is n so h = h-2n
+# dmenu2 doesn't appear to have fuzzy matching like it claims
