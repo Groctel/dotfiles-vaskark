97b04af1c0e852547f6f01cd89311ff8a761265f
vaskark <vaskark@gmail.com>
2020-03-15 18:12:58 -0400

    updates

--- .local/bin/pb-menu.sh
+++ .local/bin/pb-menu.sh
@@ -2,11 +2,9 @@
 
 ## Polybar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 5 -font "scientifica bold 8" <<< "Default|2|2b|Colors|OFF")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 5 -font "scientifica bold 8" <<< "Default|Colors|OFF")"
             case "$MENU" in
                 *Default) polybar.sh ;;
-                *2) polybar-2.sh ;;
-                *2b) polybar-2b.sh ;;
                 *Colors) polybar-p11.sh ;;
 		*OFF) pkill polybar
             esac
