bbd8c679a0f1ee7bb714c09afea371a5e9579d0c
vaskark <vaskark@gmail.com>
2020-03-19 17:01:31 -0400

    updates

---
+++ .config/wal/templates/colors-berry.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Set berry wm colors:
+berryc inner_focus_color   {color3.strip}
+berryc text_focus_color    000000
+berryc inner_unfocus_color {color0.strip}
+berryc text_unfocus_color  {color3.strip}
