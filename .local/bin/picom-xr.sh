01b5521b2082b44ebb56ba9db29f4b7541d97270
vaskark <vaskark@gmail.com>
2020-03-21 03:01:57 -0400

    updates

--- .local/bin/picom-xr.sh
+++ .local/bin/picom-xr.sh
@@ -3,5 +3,8 @@
 # Terminate
 killall -q picom
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+
 # Launch
 picom --config ~/.config/picom/picom-xr.conf >> /tmp/picom.log 2>&1 &
