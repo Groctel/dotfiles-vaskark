6bf4cf3f266ef61c3ec3ff57fb7b5c7712fbb25d
vaskark <vaskark@gmail.com>
2020-03-21 02:59:41 -0400

    updates

--- .local/bin/picom-blur.sh
+++ .local/bin/picom-blur.sh
@@ -3,5 +3,8 @@
 # Terminate
 killall -q picom
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+
 # Launch
 picom --experimental-backends --config ~/.config/picom/picom-blur.conf >> /tmp/picom.log 2>&1 &
