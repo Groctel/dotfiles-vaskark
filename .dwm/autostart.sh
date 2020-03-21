165cd219c99ffd2fad9da709367676b8bf35a0cd
vaskark <vaskark@gmail.com>
2020-03-21 19:31:00 -0400

    updates

---
+++ .dwm/autostart.sh
@@ -0,0 +1,15 @@
+#! /bin/bash 
+#nitrogen --restore &
+pulseaudio --start --log-target=syslog &
+redshift.sh &
+xautolock.sh &
+picom.sh &
+#dunst &
+libinput-gestures &
+wal -R &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
+while true; do
+	xsetroot -name "$( date +"%I:%M %p" )"
+	sleep 1m
+done &
