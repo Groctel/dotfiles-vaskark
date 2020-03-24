8220f6b9b5d6511c0bc257f192aa81714ebc0f12
vaskark <vaskark@gmail.com>
2020-03-24 18:51:50 -0400

    updates

--- .local/bin/pb-toggle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Toggle polybar:
-# If on, turn off. If off, start.
-xdotool search polybar &>/dev/null && (ps x | grep "polybar" | grep -v grep | awk '{print $1}' | xargs kill) || pb-menu.sh &
--- .local/bin/polybar-hide.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xdo hide -N "Polybar"
