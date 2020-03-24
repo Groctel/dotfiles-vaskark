6f2c7fef61b1513979741ea4060e8ab3ec47f1c5
vaskark <vaskark@gmail.com>
2020-03-24 04:41:42 -0400

    updates

--- .local/bin/picom-xr.sh
+++ .local/bin/picom-xr.sh
@@ -5,7 +5,7 @@ if ps -A | grep picom; then
 	killall -q picom
 fi
 
-sleep 1s
+sleep 0.1s
 
 # Launch
 picom --config ~/.config/picom/picom-xr.conf >> /tmp/picom.log 2>&1 &
--- .local/bin/picom.sh
+++ .local/bin/picom.sh
@@ -5,7 +5,7 @@ if ps -A | grep picom; then
 	killall -q picom
 fi
 
-sleep 1s
+sleep 0.1s
 
 # Launch
 picom --dbus --experimental-backends --config ~/.config/picom/picom.conf >> /tmp/picom.log 2>&1 &
