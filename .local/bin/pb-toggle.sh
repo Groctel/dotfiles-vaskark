312242f73a88eb943eaf70705ab327dba5899771
vaskark <vaskark@gmail.com>
2020-03-24 15:08:18 -0400

    updates

--- .local/bin/pb-toggle.sh
+++ .local/bin/pb-toggle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Toggle polybar:
 # If on, turn off. If off, start.
-xdotool search polybar &>/dev/null && (ps x | grep "polybar" | grep -v grep | awk '{print $1}' | xargs kill) || polybar-main-small.sh &
+xdotool search polybar &>/dev/null && (ps x | grep "polybar" | grep -v grep | awk '{print $1}' | xargs kill || polybar -r &
---
+++ .local/bin/polybar-show.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xdo show -N "Polybar"
