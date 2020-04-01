2b37bc81e73734d33cc91266b327d0b08c0f3260
vaskark <vaskark@gmail.com>
2020-04-01 00:11:16 -0400

    updates

--- .config/wal/templates/colors-berry.sh
+++ .config/wal/templates/colors-berry.sh
@@ -5,3 +5,5 @@ berryc inner_focus_color   {color3.strip}
 berryc text_focus_color    000000
 berryc inner_unfocus_color {color0.strip}
 berryc text_unfocus_color  {color3.strip}
+
+chmod +x ~/.cache/wal/colors-berry.sh
