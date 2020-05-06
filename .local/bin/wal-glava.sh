2c83362e257c46084f70bc2fe01bdf71d74fd103
vaskark <vaskark@gmail.com>
2020-05-05 20:19:40 -0400

    updates

--- .local/bin/wal-glava.sh
+++ .local/bin/wal-glava.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # get colors
-color=$(sed -n 4p ~/.cache/wal/colors)
+colora=$(sed -n 4p ~/.cache/wal/colors)
+colorb=$(sed -n 12p ~/.cache/wal/colors)
 
 # write chosen colors to file
-sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($color, $color, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
+sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($colora, $colorb, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
 
 # restart glava
 killall -q glava
