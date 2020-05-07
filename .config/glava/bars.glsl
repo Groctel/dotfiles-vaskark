a02dab4b307d1482c4a266a2d35de0e6d0865d05
vaskark <vaskark@gmail.com>
2020-05-07 17:16:56 -0400

    updates

--- .config/glava/bars.glsl
+++ .config/glava/bars.glsl
@@ -15,9 +15,9 @@
    correctly on X11 with `"native"` transparency */
 #define USE_ALPHA 1
 /* How quickly the gradient transitions, in pixels */
-#define GRADIENT 50
+#define GRADIENT 80
 /* Bar color. By default this provides a blue-white gradient. */
-#define COLOR @fg:mix(#97B19CFF, #97B19C, clamp(d / GRADIENT, 0, 1))
+#define COLOR @fg:mix(#994336FF, #d44d38AA, clamp(d / GRADIENT, 0, 1))
 /* Outline color. By default this provides a 'glint' outline based on the bar color */
 #define BAR_OUTLINE @bg:vec4(COLOR.rgb * 1.5, COLOR.a)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
