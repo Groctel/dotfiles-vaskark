6bd9d7c25b009236e1f6b5a43b174acb60000b1f
vaskark <vaskark@gmail.com>
2020-05-09 20:01:30 -0400

    updates

--- .local/bin/polybar-main-small-bottom.sh
+++ .local/bin/polybar-main-small-bottom.sh
@@ -5,6 +5,6 @@ killall -q polybar
 
 # Launch
 echo "---" | tee -a /tmp/polybar.log
-polybar -r -c ~/.config/polybar/config main-small-bottom >>/tmp/polybar.log 2>&1 &
+polybar -r -c ~/.config/polybar/config main-small-bottom -l info >>/tmp/polybar.log 2>&1 &
 
 #echo "Bars launched..."
