6aa4d9581458d75dc9c5e2df59d1c0187ede363e
vaskark <vaskark@gmail.com>
2020-05-15 14:27:27 -0400

    updates

--- .local/bin/wal-glava-b.sh
+++ .local/bin/wal-glava-b.sh
@@ -6,7 +6,7 @@ colorb=$(sed -n 12p ~/.cache/wal/colors)AA
 
 # write chosen colors to file
 sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($colora, $colorb, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
-sed -i "s|#define FLIP.*$|#define FLIP 1|g" ~/.config/glava/bars.glsl
+sed -i "s|#define FLIP.*$|#define FLIP 0|g" ~/.config/glava/bars.glsl
 
 # restart glava
 killall -q glava
