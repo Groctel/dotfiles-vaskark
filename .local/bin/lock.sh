dc9329630afad632b9f431c6a46cb76d7a9cf416
vaskark <vaskark@gmail.com>
2020-05-15 11:13:23 -0400

    updates

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-# Import pwwal colors
-#. "${HOME}/.cache/wal/colors.sh"
+# format colours
+color0=$(awk 'NR==1 {print substr($1,2,7)}' ~/.cache/wal/colors)
+color0a="${color0}dd"
+color3=$(awk 'NR==4 {print substr($1,2,7)}' ~/.cache/wal/colors)
+color3a="${color3}ff"
 
+# current wallpaper
+# wp1="$HOME/Pictures/"
+# wp2=$(wpg -c)
+# wp="$wp1$wp2"
+
+# i3lock command
 i3lock \
 \
--i $HOME/Pictures/trail.jpg \
+-c $color0a \
 \
 -u \
 -b \
@@ -13,8 +22,13 @@ i3lock \
 --clock \
 --timestr="%-I:%M %P" \
 --time-font="scientifica" \
+--timesize=16 \
+--timecolor=$color3a \
+\
 --datestr="" \
 \
 --greetertext="Locked!" \
 --greeter-font="Steve" \
---greetersize=50 \
+--greetersize=34 \
+--greeterpos="965:510" \
+--greetercolor=$color3a
