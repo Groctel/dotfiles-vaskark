c5f078c2d3efe2c10745d5be05e73a1496dc9458
vaskark <vaskark@gmail.com>
2020-05-16 09:36:15 -0400

    updates

--- .local/bin/lockwp.sh
+++ .local/bin/lockwp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp $HOME/Pictures/$(wpg -c) /tmp/lock.jpg
-convert /tmp/lock.jpg -blur 0x50 -fill black -colorize 50% /tmp/lockblur.jpg
+convert /tmp/lock.jpg -blur 0x50 -fill black -colorize 30% /tmp/lockblur.jpg
