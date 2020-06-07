0574aaaf50f0b8ee8b95d49d5ef20b58f322475a
vaskark <vaskark@gmail.com>
2020-06-06 20:32:57 -0400

    updates

--- .local/bin/polybar-menu.sh
+++ .local/bin/polybar-menu.sh
@@ -2,12 +2,10 @@
 
 ## Polybar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 6 -font "scientifica bold 8" <<< "i3|big|small|bottom|shades|off")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 -font "scientifica bold 8" <<< "i3|bottom|shades|off")"
             case "$MENU" in
-                *i3) polybar-main-i3.sh ;;
-		*big) polybar-main-big.sh ;;
-		*small) polybar-main-small.sh ;;
-		*bottom) polybar-main-small-bottom.sh ;;
+                *i3) polybar-i3.sh ;;
+		*bottom) polybar-bottom.sh ;;
                 *shades) polybar-p11.sh ;;
 		*off) pkill polybar
             esac
