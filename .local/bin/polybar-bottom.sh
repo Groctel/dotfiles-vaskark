4517fdeb5448c739982d082806af1f13f38f583b
vaskark <vaskark@gmail.com>
2020-06-06 20:33:24 -0400

    updates

---
+++ .local/bin/polybar-bottom.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch
+echo "---" | tee -a /tmp/polybar.log
+polybar -r -c ~/.config/polybar/config main >>/tmp/polybar.log 2>&1 &
+
+#echo "Bars launched..."
---
+++ .local/bin/wal-glava-b.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# get colors
+color2=$(sed -n 3p ~/.cache/wal/colors)FF
+color10=$(sed -n 11p ~/.cache/wal/colors)AA
+
+# write chosen colors to bars
+sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($color2, $color10, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
+sed -i "s|#define FLIP.*$|#define FLIP 0|g" ~/.config/glava/bars.glsl
+
+# radial
+sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($color2, $color10, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/radial.glsl
+
+# restart glava
+killall -q glava
+while pgrep -u $UID -x glava >/dev/null; do sleep 0.1; done
+#glava -d m bars --audio=fifo &
+glava -d -m bars
