2bd32d74bb59d692c0cc063104b956339e6a6760
vaskark <vaskark@gmail.com>
2020-07-22 19:28:03 -0400

    updates

--- .config/glava/radial.glsl
+++ .config/glava/radial.glsl
@@ -13,7 +13,7 @@
 /* How quickly the gradient transitions, in pixels */
 #define GRADIENT 50
 /* Bar color. This is a gradient by default. */
-#define COLOR @fg:mix(#686c69FF, #789a84AA, clamp(d / GRADIENT, 0, 1))
+#define COLOR @fg:mix(#68786cFF, #68786cAA, clamp(d / GRADIENT, 0, 1))
 /* Angle (in radians) for how much to rotate the visualizer */
 #define ROTATE (PI / 2)
 /* Whether to swap left/right audio buffers, set to 1 to enable */
