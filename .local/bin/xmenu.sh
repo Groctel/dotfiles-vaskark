6782e0e17a29ba08b4407f6a703e9e1c3e3c13a2
vaskark <vaskark@gmail.com>
2020-08-09 23:10:39 -0400

    updates

--- .local/bin/xmenu.sh
+++ .local/bin/xmenu.sh
@@ -1,29 +1,54 @@
 #!/bin/sh
 
 cat <<EOF | xmenu | sh &
-Audio
-	Audacity	audacity
-	Rhythmbox	rhythmbox
-Graphics
-	GIMP		gimp
-Files
+Apps
+	Appearance	lxappearance
+	Bleachbit	bleachbit
+	Calculator	gnome-calculator
+	Calendar	gnome-calendar
+	Foliate	foliate
+	Font Manager	font-manager
+	Gitg		gitg
+	Meld		meld
 	Nautilus	nautilus
 	Nemo		nemo
+	Plank		plank
+	Text Editor	gedit
+	Weather	gnome-weather
+	Zathura	zathura
+Graphics
+	GIMP		gimp
+	Inkscape	inkscape
+	Pick		pick-colour-picker
+	YACReader	yacreader
 Internet
 	Chrome		google-chrome
 	Firefox	firefox
 	Transmission	transmission-gtk
+Sound
+	Audacity	audacity
+	Easytag	easytag
+	PulseAudio	pavucontrol
+	Rhythmbox	rhythmbox
 System
 	Bleachbit	pkexec bleachbit
-	Nemo		pkexec nemo
-	Synaptic	pkexec synaptic
-Term
+	Firewall	gufw-pkexec
+	Logs		gnome-system-log-pkexec
+	Monitor	gnome-system-monitor
+	Nautilus	nautilus-pkexec
+	Nemo		nemo-pkexec
+	Stacer		stacer
+	Synaptic	synaptic-pkexec
+Terms
 	Alacritty	alacritty
+	Byobu		byobu
 	Kitty		kitty
 	Termite	termite
 	St		st
 Video
 	Celluloid	celluloid
+	MKVToolnix	mkvtoolnix-gui
+	Peek		peek
 	VLC		vlc
 
 Reboot			reboot
