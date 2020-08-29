b28aeddec1b225a08bce28b46a4768ef6430ea3a
vaskark <vaskark@gmail.com>
2020-08-28 22:49:14 -0400

    updates

---
+++ .local/bin/wal-extras.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## Author:	JST
+## Email:	jtrickett@gmail.com
+## Purpose:	Script for additional apps for pywal	
+
+## Get colors
+color00=$(sed -n 1p ~/.cache/wal/colors)
+color01=$(sed -n 2p ~/.cache/wal/colors)
+color02=$(sed -n 3p ~/.cache/wal/colors)
+color03=$(sed -n 4p ~/.cache/wal/colors)
+color04=$(sed -n 5p ~/.cache/wal/colors)
+color05=$(sed -n 6p ~/.cache/wal/colors)
+color06=$(sed -n 7p ~/.cache/wal/colors)
+color07=$(sed -n 8p ~/.cache/wal/colors)
+color08=$(sed -n 9p ~/.cache/wal/colors)
+color09=$(sed -n 10p ~/.cache/wal/colors)
+color10=$(sed -n 11p ~/.cache/wal/colors)
+color11=$(sed -n 12p ~/.cache/wal/colors)
+color12=$(sed -n 13p ~/.cache/wal/colors)
+color13=$(sed -n 14p ~/.cache/wal/colors)
+color14=$(sed -n 15p ~/.cache/wal/colors)
+color15=$(sed -n 16p ~/.cache/wal/colors)
+
+## Berry
+
+color0b=$(awk 'NR==1 {print substr($1,2,7)}' ~/.cache/wal/colors)
+color2b=$(awk 'NR==3 {print substr($1,2,7)}' ~/.cache/wal/colors)
+
+berryc inner_focus_color   $color2b
+berryc text_focus_color    $color0b
+berryc inner_unfocus_color $color0b
+berryc text_unfocus_color  $color2b
+
+## Cava
+sed -i "s|gradient_color_1.*$|gradient_color_1 = '$color2'|g" ~/.config/cava/config
+sed -i "s|gradient_color_2.*$|gradient_color_2 = '$color10'|g" ~/.config/cava/config
+
+## GLava
+
+color2g=$(sed -n 3p ~/.cache/wal/colors)FF # alpha
+color10g=$(sed -n 11p ~/.cache/wal/colors)AA # alpha
+
+sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($color2g, $color10g, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
+sed -i "s|#define FLIP.*$|#define FLIP 1|g" ~/.config/glava/bars.glsl
+
+# Check wm to decide orientation
+# if 'berry' then
+#	sed -i "s|#define FLIP.*$|#define FLIP 1|g" ~/.config/glava/bars.glsl
+# else
+# 	sed -i "s|#define FLIP.*$|#define FLIP 0|g" ~/.config/glava/bars.glsl
+# fi
+
+killall -q glava
+glava -d -m bars -a fifo &
