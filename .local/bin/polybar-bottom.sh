0879aa75216ee41e7b73a8dd5007c21d6f5c20ff
vaskark <vaskark@gmail.com>
2020-08-29 05:14:18 -0400

    updates

--- .local/bin/polybar-bottom.sh
+++ .local/bin/polybar-bottom.sh
@@ -5,6 +5,7 @@ pkill polybar
 
 # Launch
 echo "---" | tee -a /tmp/polybar.log
-polybar -r -c ~/.config/polybar/config main >>/tmp/polybar.log 2>&1 &
+polybar -r -c ~/.config/polybar/config top >>/tmp/polybar.log 2>&1 &
+polybar -r -c ~/.config/polybar/config bottom >>/tmp/polybar.log 2>&1 &
 
 #echo "Bars launched..."
