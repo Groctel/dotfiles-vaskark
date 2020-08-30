ab1299d19ce18e8b238d449db1668a2bf5cef5c6
vaskark <vaskark@gmail.com>
2020-08-30 15:48:55 -0400

    updates

--- .config/glava/bars.glsl
+++ .config/glava/bars.glsl
@@ -17,7 +17,7 @@
 /* How quickly the gradient transitions, in pixels */
 #define GRADIENT 50
 /* Bar color. By default this provides a blue-white gradient. */
-#define COLOR @fg:mix(#80b212FF, #abf50aAA, clamp(d / GRADIENT, 0, 1))
+#define COLOR @fg:mix(#809f7bFF, #99e08dAA, clamp(d / GRADIENT, 0, 1))
 /* Outline color. By default this provides a 'glint' outline based on the bar color */
 #define BAR_OUTLINE @bg:vec4(COLOR.rgb * 1.5, COLOR.a)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
