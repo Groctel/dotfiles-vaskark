6db0861157cdf78d6f246fe0070ab54789e85617
vaskark <vaskark@gmail.com>
2020-08-31 01:24:47 -0400

    updates

--- .local/bin/polybar-menu.sh
+++ .local/bin/polybar-menu.sh
@@ -2,9 +2,9 @@
 
 ## Polybar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 3 -font "scientifica bold 8" <<< "i3|main|off")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 3 -font "scientifica bold 8" <<< "main|i3|off")"
             case "$MENU" in
+		*main) polybar-main.sh ;;
                 *i3) polybar-i3.sh ;;
-		*bottom) polybar-main.sh ;;
 		*off) killall -9 polybar ;;
             esac
