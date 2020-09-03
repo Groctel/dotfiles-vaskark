aac6b8c6e6c9fde4ba290da2b84636f6d7dc90a9
vaskark <vaskark@gmail.com>
2020-09-03 02:36:36 -0400

    updates

--- .local/bin/wal-cava.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# get colors
-color2=$(sed -n 3p ~/.cache/wal/colors)
-color10=$(sed -n 11p ~/.cache/wal/colors)
-
-sed -i "s|gradient_color_1.*$|gradient_color_1 = '$color2'|g" ~/.config/cava/config
-sed -i "s|gradient_color_2.*$|gradient_color_2 = '$color10'|g" ~/.config/cava/config
