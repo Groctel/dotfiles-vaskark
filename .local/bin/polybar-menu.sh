305a843026a468a950317e780e95011c1797e259
vaskark <vaskark@gmail.com>
2020-09-05 21:20:25 -0400

    updates

--- .local/bin/polybar-menu.sh
+++ .local/bin/polybar-menu.sh
@@ -2,11 +2,13 @@
 
 ## Polybar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 5 -font "scientifica bold 8" <<< "main|mini|i3-gaps|restart|off")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Polybars' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 6 -font "scientifica bold 8" <<< "main|mini|i3-gaps|-----|restart|off")"
+
             case "$MENU" in
 		*main) polybar-main.sh ;;
 		*mini) polybar-mini.sh ;;
                 *i3-gaps) polybar-i3.sh ;;
+                *-----) ;;
                 *restart) polybar-msg cmd restart ;;
 		*off) polybar-msg cmd quit ;;
             esac
