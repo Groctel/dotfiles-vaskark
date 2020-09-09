0ffcc2adda9c6d3570f204f4bf6723fe3afb4f13
vaskark <vaskark@gmail.com>
2020-09-08 23:29:53 -0400

    updates

--- .local/bin/wal-glava-b.sh
+++ .local/bin/wal-glava-b.sh
@@ -9,5 +9,7 @@ sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($color2, $color10, clamp(d / GR
 sed -i "s|#define FLIP.*$|#define FLIP 0|g" ~/.config/glava/bars.glsl
 
 # restart glava
-killall -q glava
+if ps -A | grep glava; then
+	killall -q glava
+fi
 glava -d -a fifo -e rc-i3.glsl &
