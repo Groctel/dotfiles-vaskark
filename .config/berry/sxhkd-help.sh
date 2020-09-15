00b0146c4fff7aa35d58b1dea3300505f704798a
vaskark <vaskark@gmail.com>
2020-09-15 01:46:13 -0400

    updates

---
+++ .config/berry/sxhkd-help.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' ~/.config/berry/sxhkdrc ~/.config/sxhkd/sxhkdrc.common |
+    column -t -s $'\t' |
+    rofi -dmenu -i -markup-rows -no-show-icons -width 400 -lines 15 -yoffset 40 -p 'berry'
