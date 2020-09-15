b3371640ebe2bc133bf47ef88ecf60e513437b4d
vaskark <vaskark@gmail.com>
2020-09-15 18:40:19 -0400

    updates

---
+++ .local/bin/polybar-bspwm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+if ps -A | grep polybar; then
+	killall -q polybar
+fi
+
+# Launch
+echo "---" | tee -a /tmp/polybar.log
+polybar -r -c ~/.config/bspwm/polybar/config.bspwm main >>/tmp/polybar.log 2>&1 &
+
+#echo "Bars launched..."
