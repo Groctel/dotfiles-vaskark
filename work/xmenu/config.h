fcaf33faa864ef680d274e1e8ac07c3eb4d8465c
vaskark <vaskark@gmail.com>
2020-09-17 18:07:07 -0400

    updates

--- work/xmenu/config.h
+++ work/xmenu/config.h
@@ -1,6 +1,6 @@
 static struct Config config = {
 	/* font, separate different fonts with comma */
-	.font = "scientifica:bold:size=8",
+	.font = "scientifica:bold:size=8,DejaVuSansMono:size=8",
 
 	/* colors */
 	.background_color = "#FFFFFF",
@@ -11,11 +11,11 @@ static struct Config config = {
 	.border_color = "#E6E6E6",
 
 	/* sizes in pixels */
-	.width_pixels = 100,        /* minimum width of a menu */
-	.height_pixels = 20,        /* height of a single menu item */
+	.width_pixels = 130,        /* minimum width of a menu */
+	.height_pixels = 25,        /* height of a single menu item */
 	.border_pixels = 5,         /* menu border */
-	.separator_pixels = 15,      /* space around separator */
-	.gap_pixels = 10,           /* gap between menus */
+	.separator_pixels = 10,      /* space around separator */
+	.gap_pixels = 10,            /* gap between menus */
 
 	/*
 	 * The variables below cannot be set by X resources.
@@ -32,3 +32,28 @@ static struct Config config = {
 	/* area around the icon, the triangle and the separator */
 	.horzpadding = 8,
 };
+
+/*
+ * KEYBINDINGS
+ *
+ * Look at your /usr/include/X11/keysymdef.h  (or the equivalent file
+ * in your system) for a list of key symbol constants, and change the
+ * macros below accordingly.  Note there IS NO equal sign (=) between
+ * the macros and their values.   All key symbol constants begin with
+ * the prefix XK_
+ *
+ * For example, to use vim-like key bindings, set KEYSYMLEFT to XK_h,
+ * KEYSYMDOWN to XK_j, KEYSYMUP to XK_k, etc.
+ *
+ * Note that the regular keys like ArrowUp, ArrowDown, Tab, Home, etc
+ * will ALWAYS work, so you do not need to set them.
+ *
+ * If you do not want to set a key binding, keep it with the value of
+ * XK_VoidSymbol
+ */
+#define KSYMFIRST   XK_g      		/* select first item */
+#define KSYMLAST    XK_G       	/* select last item */
+#define KSYMUP      XK_k       	/* select previous item */
+#define KSYMDOWN    XK_j       	/* select next item */
+#define KSYMLEFT    XK_h       	/* close current menu */
+#define KSYMRIGHT   XK_Return       	/* enter selected item */
