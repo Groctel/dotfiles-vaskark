78f44aec601e043d45c1c93718b9b65aa9fd7173
vaskark <vaskark@gmail.com>
2020-09-29 21:19:43 -0400

    updates

--- work/xmenu/config.h
+++ work/xmenu/config.h
@@ -1,6 +1,6 @@
 static struct Config config = {
 	/* font, separate different fonts with comma */
-	.font = "monospace:size=9,DejaVuSansMono:size=9",
+	.font = "scientifica:bold:size=8,DejaVuSansMono:size=9",
 
 	/* colors */
 	.background_color = "#FFFFFF",
@@ -12,10 +12,10 @@ static struct Config config = {
 
 	/* sizes in pixels */
 	.width_pixels = 130,        /* minimum width of a menu */
-	.height_pixels = 25,        /* height of a single menu item */
-	.border_pixels = 1,         /* menu border */
-	.separator_pixels = 3,      /* space around separator */
-	.gap_pixels = 0,            /* gap between menus */
+	.height_pixels = 20,        /* height of a single menu item */
+	.border_pixels = 5,         /* menu border */
+	.separator_pixels = 15,      /* space around separator */
+	.gap_pixels = 10,            /* gap between menus */
 
 	/*
 	 * The variables below cannot be set by X resources.
@@ -50,9 +50,9 @@ static struct Config config = {
  * If you do not want to set a key binding, keep it with the value of
  * XK_VoidSymbol
  */
-#define KSYMFIRST   XK_VoidSymbol       /* select first item */
-#define KSYMLAST    XK_VoidSymbol       /* select last item */
-#define KSYMUP      XK_VoidSymbol       /* select previous item */
-#define KSYMDOWN    XK_VoidSymbol       /* select next item */
-#define KSYMLEFT    XK_VoidSymbol       /* close current menu */
-#define KSYMRIGHT   XK_VoidSymbol       /* enter selected item */
+#define KSYMFIRST   XK_g       /* select first item */
+#define KSYMLAST    XK_G       /* select last item */
+#define KSYMUP      XK_k       /* select previous item */
+#define KSYMDOWN    XK_j       /* select next item */
+#define KSYMLEFT    XK_h       /* close current menu */
+#define KSYMRIGHT   XK_l       /* enter selected item */
