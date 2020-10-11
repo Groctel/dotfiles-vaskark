6496f15e26a214481b02bf8025e6c355fe373a1d
vaskark <vaskark@gmail.com>
2020-10-10 20:35:49 -0400

    updates

--- .config/bspwm/sxhkd-help.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' ~/.config/bspwm/sxhkdrc ~/.config/sxhkd/sxhkdrc.common |
-    column -t -s $'\t' |
-    rofi -dmenu -i -markup-rows -no-show-icons -width 400 -lines 15 -yoffset 40 -p 'help'
