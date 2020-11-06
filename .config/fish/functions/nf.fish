3b33616d1788795adb37d35f4413dcce36133b92
vaskark <vaskark@gmail.com>
2020-11-06 17:15:11 -0500

    updates

---
+++ .config/fish/functions/nf.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function nf --wraps='clear ;neofetch --off' --description 'alias nf clear;neofetch --off'
+  clear ;neofetch --off $argv;
+end
---
+++ .config/fish/functions/pf.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function pf --wraps='clear ;pfetch' --description 'alias pf clear;pfetch'
+  clear ;pfetch $argv;
+end
