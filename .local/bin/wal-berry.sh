1e9c7933473ff58535108bfdaff1a9773743f6c6
vaskark <vaskark@gmail.com>
2020-11-11 00:33:37 -0500

    updates

--- .local/bin/wal-berry.sh
+++ .local/bin/wal-berry.sh
@@ -3,10 +3,9 @@
 # Query ~/.cache/wal/colors for colors:
 color0=$(awk 'NR==1 {print substr($1,2,7)}' ~/.cache/wal/colors)
 color2=$(awk 'NR==3 {print substr($1,2,7)}' ~/.cache/wal/colors)
-color7=$(awk 'NR==8 {print substr($1,2,7)}' ~/.cache/wal/colors)
 
 # Set berry wm colors:
 berryc inner_focus_color   $color2
 berryc text_focus_color    $color0
 berryc inner_unfocus_color $color0
-berryc text_unfocus_color  $color7
+berryc text_unfocus_color  555555
