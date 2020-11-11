10be768af9b6573dc30e63eaf882b1e56fe34f79
vaskark <vaskark@gmail.com>
2020-11-11 00:36:07 -0500

    updates

--- .local/bin/wal-berry.sh
+++ .local/bin/wal-berry.sh
@@ -8,4 +8,4 @@ color2=$(awk 'NR==3 {print substr($1,2,7)}' ~/.cache/wal/colors)
 berryc inner_focus_color   $color2
 berryc text_focus_color    $color0
 berryc inner_unfocus_color $color0
-berryc text_unfocus_color  555555
+berryc text_unfocus_color  444444
