366a2427d8d60e26bac7d6ac6ddc06982db4c9d8
vaskark <vaskark@gmail.com>
2020-11-13 17:42:58 -0500

    updates

--- .local/bin/wal-glava-b.sh
+++ .local/bin/wal-glava-b.sh
@@ -8,6 +8,10 @@ color10=$(sed -n 11p ~/.cache/wal/colors)AA
 sed -i "s|#define COLOR.*$|#define COLOR @fg:mix($color2, $color10, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
 sed -i "s|#define FLIP.*$|#define FLIP 0|g" ~/.config/glava/bars.glsl
 
+# write geometry to rc.glsl
+#sed -i "s|#request setgeometry.*$|#request setgeometry 0 0 1920 1080" ~/.config/glava/rc.glsl
+#sed -i "s|#request setgeometry.*$|#request setgeometry 560 0 800 1080" ~/.config/glava/rc.glsl
+
 # restart glava
 if ps -A | grep glava; then
 	killall -q glava
