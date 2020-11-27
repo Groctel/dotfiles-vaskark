a6b20697ce33cd3901cf501c669270091d37cc2e
vaskark <vaskark@gmail.com>
2020-11-26 21:54:37 -0500

    Updates

---
+++ .local/bin/picom-xr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# If picom is running, kill it to prevent multiple instances
+if ps -A | grep picom; then
+	killall -q picom
+fi
+
+# Wait until the processes have been shut down
+sleep 0.05s
+
+# Launch
+picom --config ~/.config/picom/picom-xr.conf >> /tmp/picom.log 2>&1 &
