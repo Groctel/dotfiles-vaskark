ef9cbad4484b953988105585c7096c0928a3ec3d
vaskark <vaskark@gmail.com>
2020-12-02 11:59:56 -0500

    Updates

---
+++ .local/bin/touchegg-bspwm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f $HOME/.config/touchegg/touchegg.conf
+ln -s $HOME/.config/touchegg/touchegg-bspwm.conf $HOME/.config/touchegg/touchegg.conf
+
+killall -9 touchegg &> /dev/null
+sleep 0.5
+touchegg &
---
+++ .local/bin/touchegg-default.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f $HOME/.config/touchegg/touchegg.conf
+ln -s $HOME/.config/touchegg/touchegg-default.conf $HOME/.config/touchegg/touchegg.conf
+
+killall -9 touchegg &> /dev/null
+sleep 0.5
+touchegg &
+
