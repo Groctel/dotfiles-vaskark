002f225953d56a678e8b6f5f44b1c63f2513db82
vaskark <vaskark@gmail.com>
2020-12-03 07:20:07 -0500

    Updates

--- .local/bin/touchegg-berry.sh
+++ .local/bin/touchegg-berry.sh
@@ -4,5 +4,4 @@ rm -f $HOME/.config/touchegg/touchegg.conf
 ln -s $HOME/.config/touchegg/touchegg-berry.conf $HOME/.config/touchegg/touchegg.conf
 
 killall -9 touchegg &> /dev/null
-sleep 0.5
-touchegg &
+touchegg &> /dev/null
--- .local/bin/touchegg-default.sh
+++ .local/bin/touchegg-default.sh
@@ -4,6 +4,5 @@ rm -f $HOME/.config/touchegg/touchegg.conf
 ln -s $HOME/.config/touchegg/touchegg-default.conf $HOME/.config/touchegg/touchegg.conf
 
 killall -9 touchegg &> /dev/null
-sleep 0.5
-touchegg &
+touchegg &> /dev/null
 
