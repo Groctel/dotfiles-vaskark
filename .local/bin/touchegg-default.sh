1091d3b3aa1cb9ff082c3ba9b8f287d41690fc11
vaskark <vaskark@gmail.com>
2020-12-13 19:07:58 -0500

    Updates

--- .local/bin/touchegg-default.sh
+++ .local/bin/touchegg-default.sh
@@ -4,5 +4,6 @@ rm -f $HOME/.config/touchegg/touchegg.conf
 ln -s $HOME/.config/touchegg/touchegg-default.conf $HOME/.config/touchegg/touchegg.conf
 
 killall -9 touchegg &> /dev/null
+sleep 0.1
 touchegg &> /dev/null &
 
--- .local/bin/touchegg-sowm.sh
+++ .local/bin/touchegg-sowm.sh
@@ -4,4 +4,5 @@ rm -f $HOME/.config/touchegg/touchegg.conf
 ln -s $HOME/.config/touchegg/touchegg-sowm.conf $HOME/.config/touchegg/touchegg.conf
 
 killall -9 touchegg &> /dev/null
+sleep 0.1
 touchegg &> /dev/null &
