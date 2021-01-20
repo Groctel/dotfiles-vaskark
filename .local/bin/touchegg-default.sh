b4773059c1488f47969abf4f56745a97d82b6523
vaskark <vaskark@gmail.com>
2021-01-20 07:28:49 -0500

    Updates

--- .local/bin/touchegg-default.sh
+++ .local/bin/touchegg-default.sh
@@ -3,8 +3,8 @@
 rm -f $HOME/.config/touchegg/touchegg.conf
 ln -s $HOME/.config/touchegg/touchegg-default.conf $HOME/.config/touchegg/touchegg.conf
 
-#if ps -A | grep touchegg; then
-#	killall -q touchegg
-#fi
+if ps -A | grep touchegg; then
+	killall -q touchegg
+fi
 
-#touchegg &
+touchegg &
