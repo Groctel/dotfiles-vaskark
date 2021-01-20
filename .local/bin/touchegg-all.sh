908f12e0b1b2bfbfa91e9c0345ed8c43957d1ac0
vaskark <vaskark@gmail.com>
2021-01-20 02:30:13 -0500

    Updates

--- .local/bin/touchegg-all.sh
+++ .local/bin/touchegg-all.sh
@@ -12,13 +12,13 @@ do
     read ENV
     case $ENV in
         berry)
-            ln -s $HOME/.config/touchegg/touchegg-berry.conf $HOME/.config/touchegg/touchegg.conf
+            ln -s $HOME/.config/touchegg/touchegg-wm.conf $HOME/.config/touchegg/touchegg.conf
             ;;
         bspwm)
-            ln -s $HOME/.config/touchegg/touchegg-bspwm.conf $HOME/.config/touchegg/touchegg.conf
+            ln -s $HOME/.config/touchegg/touchegg-wm.conf $HOME/.config/touchegg/touchegg.conf
             ;;
-        sowm)
-            ln -s $HOME/.config/touchegg/touchegg-sowm.conf $HOME/.config/touchegg/touchegg.conf
+        openbox)
+            ln -s $HOME/.config/touchegg/touchegg-default.conf $HOME/.config/touchegg/touchegg.conf
             ;;
         *)
             ln -s $HOME/.config/touchegg/touchegg-default.conf $HOME/.config/touchegg/touchegg.conf
