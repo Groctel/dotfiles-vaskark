8d8cb4732d8ea85397e99c7c917b25a587777f66
vaskark <vaskark@gmail.com>
2021-01-24 12:34:46 -0500

    Updates

---
+++ .local/bin/dmenu2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Import wal colors 
+. "${HOME}/.cache/wal/colors.sh"
+
+# vars for xyw
+b=5
+width=$(xdotool getdisplaygeometry | awk '{print $1}')
+w=$(( width-(2*b) ))
+
+# dmenu
+dmenu_run \
+-p ' dmenu ' \
+-fn 'scientifica-8:bold' \
+-h 22 \
+-nb "$color2" \
+-nf "$color0" \
+-sb "$color0" \
+-sf "$color2" \
+-x $b \
+-y $b \
+-w $w \
+
+# border is n so h = h-2n
