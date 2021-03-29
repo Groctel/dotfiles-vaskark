42bbf6a6168ea2f369bde7558cfc5ffffa712ed5
vaskark <vaskark@gmail.com>
2021-03-29 01:35:44 -0400

    Updates

--- .local/bin/xmenu.sh
+++ .local/bin/xmenu.sh
@@ -6,74 +6,74 @@ Term		alacritty
 Web		google-chrome
 
 Apps
-	Appearance	lxappearance
-	Bleachbit	bleachbit
-	Calculator	gnome-calculator
-	Calendar	gnome-calendar
-	Flameshot	flameshot gui
-	Foliate		foliate
-	Font Manager	font-manager
-	Nautilus	nautilus
-	Nemo		nemo
-	Nemo (Admin)		nemo-pkexec
-	Plank		plank
-	Text Editor	gedit
-	Weather		gnome-weather
-	Zathura		zathura
+	Appearance		lxappearance
+	Bleachbit		bleachbit
+	Calculator		gnome-calculator
+	Calendar		gnome-calendar
+	Flameshot		flameshot gui
+	Foliate			com.github.johnfactotum.Foliate
+	Foliate (Library)	com.github.johnfactotum.Foliate --library
+	Font Manager		font-manager
+	Nautilus		nautilus
+	Nemo			nemo
+	Plank			plank
+	Text Editor		gedit
+	Weather			gnome-weather
+	Zathura			zathura
 Audio
-	Audacity	audacity
-	Easytag		easytag
-	PulseAudio	pavucontrol
-	Rhythmbox	rhythmbox
-	yMuse		ymuse
+	Audacity		audacity
+	Easytag			easytag
+	PulseAudio		pavucontrol
+	Rhythmbox		rhythmbox
+	yMuse			ymuse
 Dev
-	Gitg		gitg
-	Meld		meld
-	Sublime Text	subl
-	Sublime Merge	smerge
+	Gitg			gitg
+	Meld			meld
+	Sublime Text		subl
+	Sublime Merge		smerge
 Graphics
-	GIMP		gimp
-	Inkscape	inkscape
-	Pick		pick-colour-picker
-	YACReader	YACReader
+	GIMP			gimp
+	Inkscape		inkscape
+	Pick			pick-colour-picker
+	YACReader		YACReader
 Internet
-	Chrome		google-chrome
-	Firefox		firefox
-	Transmission	transmission-gtk
+	Chrome			google-chrome
+	Firefox			firefox
+	Transmission		transmission-gtk
 Office
-	LibreOffice	libreoffice
+	LibreOffice		libreoffice
 	
-	Base		lobase
-	Calc		localc
-	Draw		lodraw
-	Impress		loimpress
-	Math		lomath
-	Writer		lowriter
+	Base			lobase
+	Calc			localc
+	Draw			lodraw
+	Impress			loimpress
+	Math			lomath
+	Writer			lowriter
 System
 	Bleachbit (Admin)	bleachbit-pkexec
-	Boxes		gnome-boxes
-	Firewall	gufw-pkexec
-	Logs		gnome-system-log-pkexec
-	Gnome Logs	gnome-logs
-	Monitor		gnome-system-monitor
-	Nautilus	nautilus-pkexec
-	Nemo		nemo-pkexec
-	Stacer		stacer
-	Synaptic	synaptic-pkexec
+	Boxes			gnome-boxes
+	Firewall		gufw-pkexec
+	Logs			gnome-system-log-pkexec
+	Gnome Logs		gnome-logs
+	Monitor			gnome-system-monitor
+	Nautilus (Admin)	nautilus-pkexec
+	Nemo (Admin)		nemo-pkexec
+	Stacer			stacer
+	Synaptic		synaptic-pkexec
 Terms
-	Alacritty	alacritty
-	Kitty		kitty
-	St		st
-	Termite		termite
-	URxvt		urxvt
-	XTerm		xterm
+	Alacritty		alacritty
+	Kitty			kitty
+	St			st
+	Termite			termite
+	URxvt			urxvt
+	XTerm			xterm
 Video
-	Celluloid	celluloid
-	MKVToolnix	mkvtoolnix-gui
-	MPV		mpv --player-operation-mode=pseudo-gui
-	VLC		vlc
+	Celluloid		celluloid
+	MKVToolnix		mkvtoolnix-gui
+	MPV			mpv --player-operation-mode=pseudo-gui
+	VLC			vlc
 
-Logout			pkill x
-Reboot			reboot
-Off			poweroff
+Logout				pkill x
+Reboot				reboot
+Off				poweroff
 EOF
