ac1e5c4103060c1a0f8a63af4e60fbca287270a9
vaskark <vaskark@gmail.com>
2021-04-24 15:32:07 -0400

    Updates

--- .local/bin/lemonbar.sh
+++ .local/bin/lemonbar.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+# colors
+color00=$(sed -n 1p ~/.cache/wal/colors)
+color01=$(sed -n 2p ~/.cache/wal/colors)
+color02=$(sed -n 3p ~/.cache/wal/colors)
+color03=$(sed -n 4p ~/.cache/wal/colors)
+color04=$(sed -n 5p ~/.cache/wal/colors)
+color05=$(sed -n 6p ~/.cache/wal/colors)
+color06=$(sed -n 7p ~/.cache/wal/colors)
+color07=$(sed -n 8p ~/.cache/wal/colors)
+color08=$(sed -n 9p ~/.cache/wal/colors)
+color09=$(sed -n 10p ~/.cache/wal/colors)
+color10=$(sed -n 11p ~/.cache/wal/colors)
+color11=$(sed -n 12p ~/.cache/wal/colors)
+color12=$(sed -n 13p ~/.cache/wal/colors)
+color13=$(sed -n 14p ~/.cache/wal/colors)
+color14=$(sed -n 15p ~/.cache/wal/colors)
+color15=$(sed -n 16p ~/.cache/wal/colors)
+
 # clock
 Clock() {
         DATETIME=$(date "+%-I:%M %p")
-        echo -n " $DATETIME"
+        echo -n "%{F"$color02"}%{F-} $DATETIME"
 }
 
 # battery
 Battery() {
         BATPERC=$(acpi --battery | cut -d, -f2)
-        echo "$BATPERC"
+        echo "%{F"$color03"}%{F-}$BATPERC"
 }
 
 # mpc
 Mpc() {
 	MPCCUR=$(mpc current)
-	echo " $MPCCUR"
+	echo "%{F"$color04"}%{F-} $MPCCUR"
 }
 
 # wifi
@@ -25,7 +43,7 @@ Wifi(){
 		WIFISTR=$(( ${WIFISTR} * 100 / 70))
 		ESSID=$(iwconfig wlp2s0 | grep ESSID | sed 's/ //g' | sed 's/.*://' | cut -d "\"" -f 2)
 		if [ $WIFISTR -ge 1 ] ; then
-			echo -e "${WIFISTR}%  ${ESSID}"
+			echo -e "%{F"$color05"}%{F-} ${ESSID}"
 		fi
 	fi
 }
@@ -36,14 +54,14 @@ Sound(){
 	if [[ ! -z $NOTMUTED ]] ; then
 		VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master) | sed 's/%//g')
 		if [ $VOL -ge 85 ] ; then
-			echo -e " ${VOL}%"
+			echo -e "%{F"$color06"}%{F-} ${VOL}%"
 		elif [ $VOL -ge 50 ] ; then
-			echo -e " ${VOL}%"
+			echo -e "%{F"$color07"}%{F-} ${VOL}%"
 		else
-			echo -e " ${VOL}%"
+			echo -e "%{F"$color07"}%{F-} ${VOL}%"
 		fi
 	else
-		echo -e " M"
+		echo -e "%{F"$color08"}%{F-} M"
 	fi
 }
 
