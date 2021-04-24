11c1b828e95ca486b213d9f204b946ae6d3645f5
vaskark <vaskark@gmail.com>
2021-04-24 13:03:51 -0400

    Updates

---
+++ .local/bin/lemonbar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# clock
+Clock() {
+        DATETIME=$(date "+%I:%M %p")
+        echo -n " $DATETIME"
+}
+
+# battery
+Battery() {
+        BATPERC=$(acpi --battery | cut -d, -f2)
+        echo "$BATPERC"
+}
+
+# mpc
+Mpc() {
+	MPCCUR=$(mpc current)
+	echo " $MPCCUR"
+}
+
+# load bar
+while true; do
+    echo "%{c}$(Mpc) %{r}$(Battery) $(Clock) "
+        sleep 1
+done
