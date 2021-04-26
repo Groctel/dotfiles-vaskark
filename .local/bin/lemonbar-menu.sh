fec3da3f3a1a98ad02340daaf2d86a9ccdf2aa07
vaskark <vaskark@gmail.com>
2021-04-26 16:31:27 -0400

    Updates

--- .local/bin/lemonbar-menu.sh
+++ .local/bin/lemonbar-menu.sh
@@ -2,7 +2,7 @@
 
 ## Lemonbar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbar 🍋 ' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 2 -font "scientifica bold 8" <<< "on|off")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbar:' -location 0 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 2 -font "scientifica bold 8" <<< "on|off")"
 
 case $MENU in
 	*on) lemonbar-start.sh ;;
