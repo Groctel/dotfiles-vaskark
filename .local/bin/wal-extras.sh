12d917cac757c382906d66e79d2765f3a6d58637
vaskark <vaskark@gmail.com>
2021-05-01 01:08:52 -0400

    Updates

--- .local/bin/wal-extras.sh
+++ .local/bin/wal-extras.sh
@@ -18,6 +18,17 @@ color13=$(sed -n 14p ~/.cache/wal/colors)
 color14=$(sed -n 15p ~/.cache/wal/colors)
 color15=$(sed -n 16p ~/.cache/wal/colors)
 
+# berry
+berry00=$(awk 'NR==1 {print substr($1,2,7)}' ~/.cache/wal/colors)
+berry02=$(awk 'NR==3 {print substr($1,2,7)}' ~/.cache/wal/colors)
+
+if ps -A | grep berry; then
+	berryc inner_focus_color   $berry02
+	berryc text_focus_color    $berry00
+	berryc inner_unfocus_color $berry00
+	berryc text_unfocus_color  444444
+fi
+
 ## dunst
 systemctl --user restart dunst
 
