ae63bc18856562b7ddd9e3df3aa4881ed3009c4c
vaskark <vaskark@gmail.com>
2021-05-01 08:14:32 -0400

    Updates

--- .local/bin/wal-extras.sh
+++ .local/bin/wal-extras.sh
@@ -19,10 +19,10 @@ color14=$(sed -n 15p ~/.cache/wal/colors)
 color15=$(sed -n 16p ~/.cache/wal/colors)
 
 # berry
-berry00=$(awk 'NR==1 {print substr($1,2,7)}' ~/.cache/wal/colors)
-berry02=$(awk 'NR==3 {print substr($1,2,7)}' ~/.cache/wal/colors)
-
 if ps -A | grep berry; then
+	berry00=$(awk 'NR==1 {print substr($1,2,7)}' ~/.cache/wal/colors)
+	berry02=$(awk 'NR==3 {print substr($1,2,7)}' ~/.cache/wal/colors)
+	
 	berryc inner_focus_color   $berry02
 	berryc text_focus_color    $berry00
 	berryc inner_unfocus_color $berry00
