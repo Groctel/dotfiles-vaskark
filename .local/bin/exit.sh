64ef15abbf8b138f788f7ace84dc4c8070ebe926
vaskark <vaskark@gmail.com>
2021-05-28 07:10:49 -0400

    Updates

---
+++ .local/bin/exit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# exit script
+# note: must be in 'operators' group on *bsd
+
+MENU="$(rofi -sep "|" -dmenu -i -p 'Going?' -location 0 -width 8 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 -font "scientifica bold 8" <<< "Lock|Logout|Restart|Shutdown")"
+
+case $(uname -s) in
+	Linux*)
+		case "$MENU" in
+			*Lock) lock.sh ;;
+			*Logout) pkill x ;;
+			*Restart) shutdown -r now ;;
+    		*Shutdown) shutdown -p now ;;
+		esac
+		;;
+	OpenBSD*)
+		case "$MENU" in
+			*Lock) lock.sh ;;
+			*Logout) pkill x ;;
+			*Restart) shutdown -r now ;;
+			*Shutdown) shutdown now ;;
+		esac
+		;;
+esac
