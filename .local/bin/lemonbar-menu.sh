60437fa321b2f8752e20301066b49fce526f0d10
vaskark <vaskark@gmail.com>
2021-06-14 22:10:38 -0400

    Updates

---
+++ .local/bin/lemonbar-menu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## Lemonbar menu
+
+MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbars:' -location 0 -width 10 -line-padding 4 -padding 20 -lines 4 <<< "Default|NF|-----|OFF")"
+
+case "$MENU" in
+	*Default)
+		rm -f $HOME/.local/bin/lemonbar-current 2>/dev/null
+		ln -s $HOME/.local/bin/lemonbar-start.sh $HOME/.local/bin/lemonbar-current
+		killall -9 lemonbar 2>/dev/null
+		lemonbar-current &
+	 ;;
+	*NF)
+		rm -f $HOME/.local/bin/lemonbar-current 2>/dev/null
+		ln -s $HOME/.local/bin/lemonbar-nf-start.sh $HOME/.local/bin/lemonbar-current
+		killall -9 lemonbar 2>/dev/null
+		lemonbar-current &
+	 ;;
+	*-----)
+	 ;;
+	*OFF)
+		killall -9 lemonbar 2>/dev/null
+	 ;; 
+esac
