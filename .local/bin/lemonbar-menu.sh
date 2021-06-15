47a8e67f9da32e666f648659b4bbb7a8d5b5bf70
vaskark <vaskark@gmail.com>
2021-06-15 07:41:56 -0400

    Updates

--- .local/bin/lemonbar-menu.sh
+++ .local/bin/lemonbar-menu.sh
@@ -2,7 +2,7 @@
 
 ## Lemonbar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbars:' -location 0 -width 10 -line-padding 4 -padding 20 -lines 5 <<< "Default|NF|Feather|-----|OFF")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbars:' -location 0 -width 10 -line-padding 4 -padding 20 -lines 4 <<< "Default|NF|Feather|OFF")"
 
 case "$MENU" in
 	
@@ -25,9 +25,6 @@ case "$MENU" in
 		ln -s $HOME/.local/bin/lemonbar-fr-start.sh $HOME/.local/bin/lemonbar-current
 		killall -9 lemonbar 2>/dev/null
 		lemonbar-current &
-	;;
-
-	*-----)
 	;;
 
 	*OFF)
