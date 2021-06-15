a26cea57a92a6c2fa948951f40a7df733adbe419
vaskark <vaskark@gmail.com>
2021-06-15 05:43:42 -0400

    Updates

--- .local/bin/lemonbar-menu.sh
+++ .local/bin/lemonbar-menu.sh
@@ -2,24 +2,36 @@
 
 ## Lemonbar menu
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbars:' -location 0 -width 10 -line-padding 4 -padding 20 -lines 4 <<< "Default|NF|-----|OFF")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Lemonbars:' -location 0 -width 10 -line-padding 4 -padding 20 -lines 5 <<< "Default|NF|Feather|-----|OFF")"
 
 case "$MENU" in
+	
 	*Default)
 		rm -f $HOME/.local/bin/lemonbar-current 2>/dev/null
 		ln -s $HOME/.local/bin/lemonbar-start.sh $HOME/.local/bin/lemonbar-current
 		killall -9 lemonbar 2>/dev/null
 		lemonbar-current &
-	 ;;
+	;;
+
 	*NF)
 		rm -f $HOME/.local/bin/lemonbar-current 2>/dev/null
 		ln -s $HOME/.local/bin/lemonbar-nf-start.sh $HOME/.local/bin/lemonbar-current
 		killall -9 lemonbar 2>/dev/null
 		lemonbar-current &
-	 ;;
+	;;
+
+	*Feather)
+		rm -f $HOME/.local/bin/lemonbar-current 2>/dev/null
+		ln -s $HOME/.local/bin/lemonbar-fr-start.sh $HOME/.local/bin/lemonbar-current
+		killall -9 lemonbar 2>/dev/null
+		lemonbar-current &
+	;;
+
 	*-----)
-	 ;;
+	;;
+
 	*OFF)
 		killall -9 lemonbar 2>/dev/null
-	 ;; 
+	;;
+
 esac
