14b66c00109caf9f834d414fa93516f6200261f6
vaskark <vaskark@gmail.com>
2021-07-25 22:42:24 -0400

    Updates

--- .local/bin/exit.sh
+++ .local/bin/exit.sh
@@ -3,21 +3,19 @@
 # exit script
 # note: must be in 'operators' group on *bsd
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Exit:' -location 0 -width 8 -hide-scrollbar -line-padding 4 -padding 20 -lines 3 -font "scientifica bold 8" <<< "Logout|Restart|Shutdown")"
+MENU="$(rofi -sep "|" \
+	-dmenu \
+	-i \
+	-p 'Leaving?' \
+	-location 0 \
+	-width 10 \
+	-hide-scrollbar \
+	-lines 3 \
+	-font "scientifica bold 8" \
+	<<< "Logout|Restart|Shutdown")"
 
-case $(uname -s) in
-	Linux*)
-		case "$MENU" in
-			*Logout) pkill x ;;
-			*Restart) shutdown -r now ;;
-    		*Shutdown) shutdown -p now ;;
-		esac
-		;;
-	OpenBSD*)
-		case "$MENU" in
-			*Logout) pkill x ;;
-			*Restart) shutdown -r now ;;
-			*Shutdown) shutdown now ;;
-		esac
-		;;
+case "$MENU" in
+	*Logout) pkill x ;;
+	*Restart) shutdown -r now ;;
+	*Shutdown) shutdown -p now ;;
 esac
---
+++ .local/bin/rofi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rofi \
+-show drun \
+-display-drun Apps \
+-display-run Bin \
+-display-ssh SSH \
+-display-emoji Emoji \
+-display-keys Keys \
+-location 0 \
+-width 14 \
+-hide-scrollbar \
+-lines 5 \
+-disable-history \
+-matching normal \
+-font "scientifica bold 8"
