fd9fb916dc72fa99ab6ecc6d3c5c5c63dbd9be1c
vaskark <vaskark@gmail.com>
2021-08-12 01:38:46 -0400

    Updates

--- .config/scripts/time.sh
+++ .config/scripts/time.sh
@@ -2,26 +2,21 @@
 
 # current system time
 
-[ -z "$1" ] && set -- '${t}'
-
-t=
-
 case $(uname -s) in
+
 	Linux*)
 		t=$(date +"%-I:%M %p")
+                echo "$t"
 		;;
+
 	FreeBSD*)
 		t=$(date +"%-I:%M %p")
+                echo "$t"
 		;;
+
 	OpenBSD*)
 		t=$(date +"%I:%M %p")
+                echo "$t"
 		;;
-	*)
-		#echo "Unsupported os: $(uname -s)" >&2
-        exit 1
-    	;;
+
 esac
-
-eval echo "$1"
-
-		
