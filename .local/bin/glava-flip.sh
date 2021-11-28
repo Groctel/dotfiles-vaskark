e002703f43ba361d4ad347f4b4eea421f2693fa5
vaskark <vaskark@gmail.com>
2021-11-27 23:19:32 -0500

    Updates

---
+++ .local/bin/glava-flip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GLAVA=$(pgrep -x glava)
+
+if [[ -n "${GLAVA}" ]]; then
+    
+    CURRENT=$(awk '/#define FLIP/ {print $3}' ~/.config/glava/bars.glsl)
+    if [[ "${CURRENT}" = 1 ]] ; then
+    	sed -i "s|#define FLIP.*$|#define FLIP 0|g" ~/.config/glava/bars.glsl
+    else	
+	sed -i "s|#define FLIP.*$|#define FLIP 1|g" ~/.config/glava/bars.glsl
+    fi
+    
+    pkill glava && glava -d &> /dev/null &
+
+fi
