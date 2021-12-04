4d7c2f79353609b495a0611b01060220262725f2
vaskark <vaskark@gmail.com>
2021-12-04 12:50:29 -0500

    Updates

--- .local/bin/fz-wal.sh
+++ .local/bin/fz-wal.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Lists dark themes by default.
 # Light themes are listed if an argument is passed.
 # Export FZWAL_RESET_CURSOR=1 if you don't want Pywal to change
@@ -34,7 +34,7 @@ fi
 
 if [ -n "$FZWAL_RESET_CURSOR" ]; then
     for TTY in /dev/pts/*; do
-        [ -w "$TTY" ] && /bin/printf "\e]112\a" > "$TTY"
+        [ -w $TTY ] && /bin/printf "\e]112\a" > $TTY
     done
     exit 0
 fi
