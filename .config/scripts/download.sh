e480bfd39ead22cde0b294216d1e2b3aedecbb1b
vaskark <vaskark@gmail.com>
2021-12-08 13:53:27 -0500

    Updates

---
+++ .config/scripts/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# download
+INTERFACE=$(ip addr | awk '/state UP/ {print $2}' | sed 's/://g')
+WIFISTR=$(iwconfig "$INTERFACE" | grep "Link" | sed 's/ //g' | sed 's/LinkQuality=//g' | sed 's/\/.*//g') 
+
+DWN=$(ifstat -i "$INTERFACE" 0.1s 1 | awk 'NR==3 {print $1}')
+echo "Download: $DWN Kb/s"
