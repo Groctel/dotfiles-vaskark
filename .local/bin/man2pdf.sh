2edfced75403992bd132a2fc09d7f1f9637da55c
vaskark <vaskark@gmail.com>
2022-03-15 14:59:01 -0400

    Updates

--- .local/bin/man2pdf.sh
+++ .local/bin/man2pdf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-man -k . | rofi -dmenu -p 'Man:' -l 5 -theme-str 'window {width: 15%;}' | awk '{print $1}' | xargs -r man -Tpdf | zathura -
+man -k . | rofi -dmenu -p 'Man' -l 5 -theme-str 'window {width: 15%;}' | awk '{print $1}' | xargs -r man -Tpdf | zathura -
