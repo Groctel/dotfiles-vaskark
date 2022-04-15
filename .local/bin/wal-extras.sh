cf4d9f4720ccdfb5614bb8d68e009d753ab6d24a
vaskark <vaskark@gmail.com>
2022-04-15 02:06:25 -0400

    Updates

--- .local/bin/wal-extras.sh
+++ .local/bin/wal-extras.sh
@@ -25,3 +25,8 @@ fi
 # glava
 sed -i "s|#define COLOR @fg.*$|#define COLOR @fg:mix(""$color2""ff, ""$color10""aa, clamp(d / GRADIENT, 0, 1))|g" ~/.config/glava/bars.glsl
 [ "$(pgrep -x glava)" ] && pkill glava && glava -d 2>/dev/null &
+
+# cava
+sed -i "s|gradient_color_1.*$|gradient_color_1 = \'""$color5""\'|g" ~/.config/cava/config
+sed -i "s|gradient_color_2.*$|gradient_color_2 = \'""$color13""\'|g" ~/.config/cava/config
+[ "$(pgrep -x cava)" ] && pkill -USR2 cava
