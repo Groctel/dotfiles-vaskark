d41866a9005abd92388b963815a49158adc26138
vaskark <vaskark@gmail.com>
2022-04-16 21:50:17 -0400

    Updates

--- .local/bin/polybar-menu.sh
+++ .local/bin/polybar-menu.sh
@@ -12,17 +12,17 @@ MENU="$(rofi -sep "|" \
 case "$MENU" in
 	
 	*siji)
-		ln -sf "$HOME"/.local/bin/pb.sh "$HOME"/.local/bin/polybar-current
+		ln -sf "$HOME"/.local/bin/polybar.sh "$HOME"/.local/bin/polybar-current
 		polybar-current
 	;;
 
 	*nf)
-		ln -sf "$HOME"/.local/bin/pb-nf.sh "$HOME"/.local/bin/polybar-current
+		ln -sf "$HOME"/.local/bin/polybar-nf.sh "$HOME"/.local/bin/polybar-current
 		polybar-current
 	;;
 
 	*emoji)
-		ln -sf "$HOME"/.local/bin/pb-emoji.sh "$HOME"/.local/bin/polybar-current
+		ln -sf "$HOME"/.local/bin/polybar-emoji.sh "$HOME"/.local/bin/polybar-current
 		polybar-current
 	;;
 
