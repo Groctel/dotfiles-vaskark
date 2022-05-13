1c95e5f8c6a3e9f0e4ca25a944ce381568c36b64
vaskark <vaskark@gmail.com>
2022-05-13 14:49:22 -0400

    Updates

--- .local/bin/cwm.sh
+++ .local/bin/cwm.sh
@@ -6,7 +6,7 @@ wal -R -o wal-extras.sh &
 xscreensaver --no-splash &
 xidle -program "/usr/local/bin/xscreensaver-command -lock" -timeout 300 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-systemctl --user import-environment; systemctl --user restart redshift &
+systemctl --user import-environment; systemctl --user restart redshift
 
-# wm
+# cwm
 exec /usr/local/bin/cwm
