b19c2ece2819cde5694b73c2ffbfa3d256438a59
vaskark <vaskark@gmail.com>
2022-07-10 14:09:59 -0400

    Updates

--- .local/bin/glava.sh
+++ .local/bin/glava.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-w=$(( $(xdotool getdisplaygeometry | awk '{print $1}') / 2 ))
+if [ $(pgrep -x openbox) ]; then
+	w=$(( $(xdotool getdisplaygeometry | awk '{print $1}') / 1 ))
+else
+	w=$(( $(xdotool getdisplaygeometry | awk '{print $1}') / 2 ))
+fi
+
 h=$(( $(xdotool getdisplaygeometry | awk '{print $2}') ))
 x=$(( $(xdotool getdisplaygeometry | awk '{print $1}') / 2 - (w/2) ))
 
