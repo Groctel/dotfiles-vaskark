bb01f72bcf462c897d8ae65456e2a9b432a67574
vaskark <vaskark@gmail.com>
2022-07-24 13:18:00 -0400

    Updates

--- .local/bin/lemonbar-menu.sh
+++ .local/bin/lemonbar-menu.sh
@@ -10,8 +10,8 @@ MENU="$(rofi -sep "|" \
 	-location 0 \
 	-font 'scientifica bold 8' \
 	-theme "~/.cache/wal/colors-rofi-mine.rasi" \
-    -theme-str 'window {width: 10%;} listview {lines: 2;}' \
-	<<< "siji|nf")"
+    -theme-str 'window {width: 10%;} listview {lines: 3;}' \
+	<<< "siji|nf|emoji")"
 
 case "$MENU" in
 	
@@ -23,6 +23,12 @@ case "$MENU" in
 
 	*nf)
 		ln -sf "$HOME"/.local/bin/lemonbar-nf.sh "$HOME"/.local/bin/lemonbar-current
+		pkill lemonbar
+		lemonbar-current
+	;;
+
+	*emoji)
+		ln -sf "$HOME"/.local/bin/lemonbar-emoji.sh "$HOME"/.local/bin/lemonbar-current
 		pkill lemonbar
 		lemonbar-current
 	;;
