de4e23f37b205578a31a9a642ec8147a40020bae
vaskark <vaskark@gmail.com>
2022-07-29 17:05:14 -0400

    Updates

--- .local/bin/wal-switcher.sh
+++ .local/bin/wal-switcher.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #
 # Pywal theme switcher
@@ -7,14 +7,14 @@
 theme=$(wal --theme | sed '/Light Themes/,$d' | sed '/Dark Themes/,$d' | sed -e '/^\S/d' -e 's/ - //' | rofi \
     -dmenu \
     -i \
-    -p 'Themes' \
+    -p 'Pywal' \
     -hide-scrollbar \
     -matching normal \
     -font 'scientifica bold 8' \
     -theme "$HOME/.cache/wal/colors-rofi-mine.rasi" \
-    -theme-str 'window {width: 12%;} listview {lines: 4; columns: 1;}')
+    -theme-str 'window {width: 12%;} listview {lines: 4; columns: 1;}'
+)
 
-if [ "$theme" ]; then
-	wal -q --theme $theme -o wal-extras.sh 2>/dev/null
+if [ -n "$theme" ]; then
+	wal -q --theme "$theme" -o wal-extras.sh
 fi
-
