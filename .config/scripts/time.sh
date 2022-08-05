0570eb1ac2d791741595de07dd5216fb10d311d7
vaskark <vaskark@gmail.com>
2022-08-04 20:38:21 -0400

    Updates

--- .config/scripts/time.sh
+++ .config/scripts/time.sh
@@ -6,17 +6,16 @@ case $(uname -s) in
 
 	Linux*)
 		t=$(date +"%-I:%M %p")
-                echo "$t"
 		;;
 
 	FreeBSD*)
 		t=$(date +"%-I:%M %p")
-                echo "$t"
 		;;
 
 	OpenBSD*)
 		t=$(date +"%I:%M %p")
-                echo "$t"
 		;;
 
 esac
+
+echo "$t"
